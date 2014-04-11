0064ba0c07139bf305dc1c6cc7ba3f0674710125
Nick DiLeo <ndileo01@villanova.edu>
2014-04-11 16:50:58 -0400

    Added directory for MrT SLAM from data_collect.  This commit is prior to making angle changes.

--- SimpleSLAM-master/how_to_cluster_data.m
+++ SimpleSLAM-master/how_to_cluster_data.m
@@ -1,15 +1,21 @@
 %Example of clustering all the landmark estimates returned from fastSLAM
 
 %create a matrix of all landmark estimates estimates from each particle
+tic 
 row_counter=0;
+% landmarks_position = [0 0 0];
 for pIdx=1:num_particles
    for j=1:particles(pIdx).num_landmarks(end) %this should be index timestep when in full script
        row_counter=row_counter+1;
-       landmarks_positions(row_counter,:)=particles(pIdx).landmarks(j).pos(1:2)';
+       landmarks_positions(row_counter,:)=particles(pIdx).landmarks(j).pos(1:2)'; 
    end
-    
+   
+%    lm_from_particles=[particles(pIdx).landmarks.pos]';
+%    landmarks_position(end+1:end+size(lm_from_particles,1),:)=lm_from_particles;
+%    
+%    
 end
-
+toc
 %clusters data together to get an estimate of landmark positions
 %the sensitivity value must be tuned to properly separate clusters.
 %this value is dependent on how far away "clusters" are from each other
